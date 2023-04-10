29958c3de6743caa9266ab895982f78b24116ee8
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2023-04-10 15:02:29 +0700

    update data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\bookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [bookController::class, 'index']);
